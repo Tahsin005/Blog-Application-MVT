c7faeb17ec8c5524cddb451501dc0a0caa60d7d8
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-23 23:04:58 +0600

    <//>

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
