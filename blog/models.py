16f721f6db1aa3c096937db58db8058b4384ba27
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-28 22:12:14 +0600

    added custom model manager fro Post model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,12 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -22,6 +28,9 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
+
+    objects = models.Manager() # default manager.
+    published = PublishedManager() # custom manager
 
     class Meta:
         ordering = ['-publish']
