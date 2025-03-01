aa9c5f72db81ee7f8b3cf3d4ef8e57d3bfdb3b91
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-03-02 02:53:08 +0600

    added commen submission form and its related implementation

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
-from blog.forms import EmailPostForm
+from blog.forms import EmailPostForm, CommentForm
+from django.views.decorators.http import require_POST
 from django.http import Http404
 from django.views.generic import ListView
 from django.core.mail import send_mail
@@ -47,11 +48,18 @@ def post_detail(request, year, month, day, post):
         publish__day=day
     )
 
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+
     return render(
         request,
         'blog/post/detail.html',
         {
-            'post': post
+            'post': post,
+            'comments': comments,
+            'form': form
         }
     )
 
@@ -99,5 +107,32 @@ def post_share(request, post_id):
             'post': post,
             'form': form,
             'sent': sent
+        }
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
         }
     )
