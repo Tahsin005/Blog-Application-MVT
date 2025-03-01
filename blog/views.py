a8a0241fc6fed60dac55dd07e28604c3a90a2297
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-03-01 21:59:42 +0600

    using a class-based view to list posts

--- blog/views.py
+++ blog/views.py
@@ -2,29 +2,39 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.http import Http404
+from django.views.generic import ListView
 
 # Create your views here.
-def post_list(request):
-    post_list = Post.published.all()
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page number is not an integer, deliver first page.
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        posts = paginator.page(paginator.num_pages)
+# def post_list(request):
+#     post_list = Post.published.all()
+#     # Pagination with 3 posts per page
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # If page number is not an integer, deliver first page.
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page is out of range (e.g. 9999), deliver last page of results.
+#         posts = paginator.page(paginator.num_pages)
 
-    return render(
-        request,
-        'blog/post/list.html',
-        {
-            'posts': posts
-        }
-    )
+#     return render(
+#         request,
+#         'blog/post/list.html',
+#         {
+#             'posts': posts
+#         }
+#     )
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
