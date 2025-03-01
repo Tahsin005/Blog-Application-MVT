22fe6312435a584a2c8fa7b626569a17e3a1d2d9
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-03-02 05:23:28 +0600

    added tagging for each post

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -18,6 +19,7 @@ class Post(models.Model):
         max_length=250,
         unique_for_date='publish'
     )
+    tags = TaggableManager()
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
--- blog/views.py
+++ blog/views.py
@@ -6,37 +6,43 @@ from django.views.decorators.http import require_POST
 from django.http import Http404
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from taggit.models import Tag
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     # Pagination with 3 posts per page
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         # If page number is not an integer, deliver first page.
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range (e.g. 9999), deliver last page of results.
-#         posts = paginator.page(paginator.num_pages)
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page number is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
 
-#     return render(
-#         request,
-#         'blog/post/list.html',
-#         {
-#             'posts': posts
-#         }
-#     )
+    return render(
+        request,
+        'blog/post/list.html',
+        {
+            'posts': posts,
+            'tag': tag
+        }
+    )
 
-class PostListView(ListView):
-    """
-    Alternative post list view
-    """
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     """
+#     Alternative post list view
+#     """
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
