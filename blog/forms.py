d4b73c67c9fd4e49d25fb42e3b82fb4be2a1421c
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-03-04 02:26:08 +0600

    added tailwind css and updated the ui

--- blog/forms.py
+++ blog/forms.py
@@ -1,19 +1,50 @@
 from django import forms
 from blog.models import Comment
 
+from django import forms
+
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(
-        required=False,
-        widget=forms.Textarea
-    )
+    name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={
+        'class': 'w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+        'placeholder': 'Your Name'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+        'placeholder': 'Your E-mail'
+    }))
+    to = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+        'placeholder': 'Recipient\'s E-mail'
+    }))
+    comments = forms.CharField(required=False, widget=forms.Textarea(attrs={
+        'class': 'w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+        'placeholder': 'Message (optional)',
+        'rows': 4
+    }))
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({
+            'class': 'mt-1 block w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500',
+            'placeholder': 'Your Name'
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'mt-1 block w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500',
+            'placeholder': 'Your Email'
+        })
+        self.fields['body'].widget.attrs.update({
+            'class': 'mt-1 block w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500',
+            'placeholder': 'Write your comment here...'
+        })
+
 class SearchForm(forms.Form):
-    query = forms.CharField()
+    query = forms.CharField(max_length=25, widget=forms.TextInput(attrs={
+        'class': 'w-full p-3 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+        'placeholder': 'Your Name'
+    }))
